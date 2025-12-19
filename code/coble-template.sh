77f15a6f9db30a9363a8d01ceb93ee092389484f
Rachel Alcraft <132372271+rachelicr@users.noreply.github.com>
2025-12-19 17:57:27 +0000

    base template

--- code/coble-template.sh
+++ code/coble-template.sh
@@ -1,1 +1,32 @@
+#!
+
+##############
+# success=$(coble-template.sh my.yml)
+##############
+# Inputs ----
+# 1. yamlfile
+# Outputs ----
+# --stdout --
+# 1. success=Y/N
+# --filesystem --
+# 1. yamlfile
+###############
+
+yamlfile=${1}
+
+# init with basic time date
+
+echo headers and version
+
+echo basic language
+
+echo flags and tools
+
+echo some basics and each category with help
+
+
+
+
+
+
 
