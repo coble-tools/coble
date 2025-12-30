a64409db420ac1c1479d09f4eee179aa930aece5
rachelicr <rachel.alcraft@icr.ac.uk>
2025-12-30 14:21:08 +0000

    try it in bin though not best practice

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -40,3 +40,7 @@ chmod +x $PREFIX/bin/coble-tml_basic.sh
 chmod +x $PREFIX/bin/coble-tml_fix.sh
 chmod +x $PREFIX/bin/coble-tml_versions.sh
 chmod +x $PREFIX/bin/coble-tml_bash.sh
+
+# Copy and executable the tests
+cp tests/conda/tests.sh $PREFIX/bin/tests.sh
+chmod +x $PREFIX/bin/tests.sh
