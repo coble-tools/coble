a1b73e2068e5e4d9f897737e324d632d41dd3e5e
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2026-01-31 09:22:17 +0000

    Clean pytest cache to save disk space in CI

--- tests/github/test_docker.py
+++ tests/github/test_docker.py
@@ -4,7 +4,7 @@ import os
 def test_coble_small_docker():
     """Test that the small env runs."""
     result = subprocess.run([
-        'bash', 'code/coble', 'build', '--recipe', 'tests/fixtures/small.cbl', '--env', 'small', '--containers', '--docker'
+        'bash', 'code/coble', 'build', '--recipe', 'tests/fixtures/small.cbl', '--env', 'small', '--containers', 'docker'
     ], cwd=os.path.dirname(os.path.dirname(os.path.dirname(__file__))), capture_output=True, text=True)
     assert result.returncode == 0
     assert 'usage' in result.stdout.lower() or 'help' in result.stdout.lower()
