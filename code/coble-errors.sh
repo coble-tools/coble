6d9d9057a90d71027bd91fe448c4936bb9ebc16f
rachelicr <rachel.alcraft@icr.ac.uk>
2026-02-09 22:41:24 +0000

    try different pytest image

--- code/coble-errors.sh
+++ code/coble-errors.sh
@@ -29,6 +29,9 @@ stdout_patterns=(
     "Failed to build"     
     "ERROR: compilation failed" 
     "HTTP error 404"
+    "ClobberError"
+    "SafetyError"
+    "LinkError: post-link script failed for package"
 )
 error_patterns=(
     "* removing"    
@@ -51,6 +54,9 @@ error_patterns=(
     "packages failed:"
     "ERROR: compilation failed" 
     "HTTP error 404"
+    "ClobberError"
+    "SafetyError"
+    "LinkError: post-link script failed for package"
 )
 done_patterns=(    
     "Successfully installed"
@@ -64,7 +70,7 @@ dep_patterns=(
     "linux-64::"
     "linux-aarch64::"
     "noarch::"    
-    " +"
+    #" +"
 )
 
 # Write patterns to temp files
---
+++ recipes/icr/carbine/README.md
@@ -0,0 +1,15 @@
+# Build for conda
+code/coble build --recipe recipes/icr/carbine/carbine.cbl \
+--env carbine \
+--validate recipes/icr/carbine/validate/validate.sh \
+--val-folder recipes/icr/carbine/validate/ \
+--rebuild
+
+# Build for docker and singularity
+code/coble build --recipe recipes/icr/carbine/carbine.cbl \
+--env carbine \
+--validate recipes/icr/carbine/validate/validate.sh \
+--val-folder recipes/icr/carbine/validate/ \
+--containers docker,singularity
+#--banner ICR
+#--target linux/amd64,linux/arm64 \
