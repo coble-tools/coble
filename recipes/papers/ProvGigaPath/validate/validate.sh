759a2f516fc9b311683370dc466f65e3a43430d6
rachelicr <rachel.alcraft@icr.ac.uk>
2026-02-28 20:54:04 +0000

    added another message about LD_PATH

--- recipes/papers/ProvGigaPath/validate/validate.sh
+++ recipes/papers/ProvGigaPath/validate/validate.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
 if [ -n "$APPTAINER_CONTAINER" ] || [ -n "$SINGULARITY_CONTAINER" ]; then
     export LD_LIBRARY_PATH=/.singularity.d/libs:$LD_LIBRARY_PATH
+    echo "Singularity container detected - LD_LIBRARY_PATH set for GPU support"
+    echo "Note: run 'export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH' if using Python directly"
 fi
-
 python ${CONDA_PREFIX}/bin/validate.py
 
 PYTHON_EXIT=$?
