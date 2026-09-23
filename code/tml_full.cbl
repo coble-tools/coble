84c45e254034faa27e3fe186c89e54513e0e0925
Rachel Alcraft <rachel.alcraft@icr.ac.uk>
2026-09-23 17:00:04 +0100

    template text changes

--- code/tml_full.cbl
+++ code/tml_full.cbl
@@ -1,9 +1,7 @@
 #####################################################
 # COBLE:Reproducible environment: BASIC, (c) ICR 2026
 #####################################################
-# This template demomnstaes all thepossible features along with the defaults
-# Note that the recipe can be called with --alias mamba to use mamba instead of conda
-# but that there is also an alias directive to set the alias within the recipe itself
+# This template demonstrates the possible features along with the defaults
 #####################################################
 coble:
   - environment: This is just a descriptive field but `capture` saves the end name
@@ -18,12 +16,10 @@ flags:
   - compile: system=false # some common system tools e.g. devtools BiocManager, remotes
   - compile: tools=false # false / 13.1 / true  - installs defaults or specified versions of common libs
   - compile: paths=true # true / false - adds compile tool paths only to conda env (not needed if you have the above)
-  - network-viz: true # true / false - installs R packages for network visualisation of R package dependencies
+  - network-viz: false # true / false - installs R packages for network visualisation of R package dependencies
   - export: VAR1=VALUE1 # sets environment variables within the conda env
   - updates: false # false / true = --no-update-deps / --update-deps or a specific flag string such as "--freeze-installed"
-  - alias: conda # sets the conda alias to use - conda / mamba / micromamba a user path etc, only fir the solver eg mamba install
   - ncpus: 4 # number of cpus to use for r package installs
-  - solver: libmamba # classic
   - cran-repo: https://packagemanager.posit.co/cran/2020-04-01 # dated cran repo to ensure reproducibility of r package installs
 conda:
   - pandas
